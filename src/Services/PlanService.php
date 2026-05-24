840a44536456668a257c375a19e17024473805fb
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2026-05-24 23:55:38 +0300

    Add CRUD operations for pricing plans and products in Iyzico

--- src/Services/PlanService.php
+++ src/Services/PlanService.php
@@ -3,8 +3,13 @@
 namespace Codenteq\Iyzico\Services;
 
 use Iyzipay\Model\Subscription\SubscriptionPricingPlan;
+use Iyzipay\Model\Subscription\RetrieveList;
 use Iyzipay\Options;
 use Iyzipay\Request\Subscription\SubscriptionCreatePricingPlanRequest;
+use Iyzipay\Request\Subscription\SubscriptionDeletePricingPlanRequest;
+use Iyzipay\Request\Subscription\SubscriptionListPricingPlanRequest;
+use Iyzipay\Request\Subscription\SubscriptionRetrievePricingPlanRequest;
+use Iyzipay\Request\Subscription\SubscriptionUpdatePricingPlanRequest;
 
 class PlanService
 {
@@ -21,8 +26,67 @@ class PlanService
     /**
      * Create a pricing plan in Iyzico
      */
-    public function create(SubscriptionCreatePricingPlanRequest $request): SubscriptionPricingPlan
+    public function create(string $productReferenceCode, array $data): SubscriptionPricingPlan
     {
+        $request = new SubscriptionCreatePricingPlanRequest;
+        $request->setProductReferenceCode($productReferenceCode);
+        $request->setName($data['name']);
+        $request->setPrice($data['price']);
+        $request->setCurrencyCode($data['currency_code']);
+        $request->setPaymentInterval($data['payment_interval']);
+        $request->setPlanPaymentType($data['plan_payment_type']);
+        $request->setPaymentIntervalCount($data['payment_interval_count']);
+        $request->setRecurrenceCount($data['recurrence_count']);
+        $request->setTrialPeriodDays($data['trial_period_days']);
+
         return SubscriptionPricingPlan::create($request, $this->options);
+    }
+
+    /**
+     * Update a pricing plan in Iyzico
+     */
+    public function update(string $pricingPlanReferenceCode, array $data): SubscriptionPricingPlan
+    {
+        $request = new SubscriptionUpdatePricingPlanRequest;
+        $request->setPricingPlanReferenceCode($pricingPlanReferenceCode);
+        $request->setName($data['name']);
+        $request->setTrialPeriodDays($data['trial_period_days']);
+
+        return SubscriptionPricingPlan::update($request, $this->options);
+    }
+
+    /**
+     * Retrieve pricing plan detail from Iyzico
+     */
+    public function retrieve(string $pricingPlanReferenceCode): SubscriptionPricingPlan
+    {
+        $request = new SubscriptionRetrievePricingPlanRequest;
+        $request->setPricingPlanReferenceCode($pricingPlanReferenceCode);
+
+        return SubscriptionPricingPlan::retrieve($request, $this->options);
+    }
+
+    /**
+     * Delete a pricing plan in Iyzico
+     */
+    public function delete(string $pricingPlanReferenceCode): SubscriptionPricingPlan
+    {
+        $request = new SubscriptionDeletePricingPlanRequest;
+        $request->setPricingPlanReferenceCode($pricingPlanReferenceCode);
+
+        return SubscriptionPricingPlan::delete($request, $this->options);
+    }
+
+    /**
+     * List pricing plans for a product in Iyzico
+     */
+    public function list(string $productReferenceCode, array $params = []): RetrieveList
+    {
+        $request = new SubscriptionListPricingPlanRequest;
+        $request->setProductReferenceCode($productReferenceCode);
+        $request->setPage($params['page']);
+        $request->setCount($params['count']);
+
+        return RetrieveList::pricingPlan($request, $this->options);
     }
 }
--- src/Services/ProductService.php
+++ src/Services/ProductService.php
@@ -3,8 +3,13 @@
 namespace Codenteq\Iyzico\Services;
 
 use Iyzipay\Model\Subscription\SubscriptionProduct;
+use Iyzipay\Model\Subscription\RetrieveList;
 use Iyzipay\Options;
 use Iyzipay\Request\Subscription\SubscriptionCreateProductRequest;
+use Iyzipay\Request\Subscription\SubscriptionDeleteProductRequest;
+use Iyzipay\Request\Subscription\SubscriptionListProductsRequest;
+use Iyzipay\Request\Subscription\SubscriptionRetrieveProductRequest;
+use Iyzipay\Request\Subscription\SubscriptionUpdateProductRequest;
 
 class ProductService
 {
@@ -21,8 +26,59 @@ class ProductService
     /**
      * Create a product in Iyzico
      */
-    public function create(SubscriptionCreateProductRequest $request): SubscriptionProduct
+    public function create(array $data): SubscriptionProduct
     {
+        $request = new SubscriptionCreateProductRequest;
+        $request->setName($data['name']);
+        $request->setDescription($data['description']);
+
         return SubscriptionProduct::create($request, $this->options);
+    }
+
+    /**
+     * Update a product in Iyzico
+     */
+    public function update(string $productReferenceCode, array $data): SubscriptionProduct
+    {
+        $request = new SubscriptionUpdateProductRequest;
+        $request->setProductReferenceCode($productReferenceCode);
+        $request->setName($data['name']);
+        $request->setDescription($data['description']);
+
+        return SubscriptionProduct::update($request, $this->options);
+    }
+
+    /**
+     * Retrieve product detail from Iyzico
+     */
+    public function retrieve(string $productReferenceCode): SubscriptionProduct
+    {
+        $request = new SubscriptionRetrieveProductRequest;
+        $request->setProductReferenceCode($productReferenceCode);
+
+        return SubscriptionProduct::retrieve($request, $this->options);
+    }
+
+    /**
+     * Delete a product in Iyzico
+     */
+    public function delete(string $productReferenceCode): SubscriptionProduct
+    {
+        $request = new SubscriptionDeleteProductRequest;
+        $request->setProductReferenceCode($productReferenceCode);
+
+        return SubscriptionProduct::delete($request, $this->options);
+    }
+
+    /**
+     * List products in Iyzico
+     */
+    public function list(array $params = []): RetrieveList
+    {
+        $request = new SubscriptionListProductsRequest;
+        $request->setPage($params['page']);
+        $request->setCount($params['count']);
+
+        return RetrieveList::products($request, $this->options);
     }
 }
